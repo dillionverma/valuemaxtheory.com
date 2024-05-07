a708a4b782406fac55f92365330f956e9f10f464
Dillion Verma <hello@dillion.io>
2024-05-06 22:11:14 -0400

    fix: update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 import { withContentlayer } from "next-contentlayer";
 
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true, swcMinify: true };
+const nextConfig = {
+  output: process.env.NODE_ENV === "production" ? "standalone" : undefined,
+  reactStrictMode: true,
+  swcMinify: true,
+};
 
 export default withContentlayer(nextConfig);
